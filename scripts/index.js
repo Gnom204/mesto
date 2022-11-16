dc1d851c764c65eec0442921fcf5d6985d2633f6
Aleksandr Sitnikov <gnomek100500@gmail.com>
2022-11-16 06:20:20 +0300

    feat: написать логику страницы

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let popupElement = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close');
+let formsZone = document.querySelector('.popup__forms');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form_name');
+let nameDescription = document.querySelector('.popup__form_description');
+let userName = document.querySelector('.profile__title');
+let userDescription = document.querySelector('.profile__subtitle');
+let saveButton = document.querySelector('.popup__save-button')
+// Функции открытия и закрытия попапа
+function popupOpen() {
+    popupElement.classList.add('popup_is-open')
+    nameInput.value = userName.textContent;
+    nameDescription.value = userDescription.textContent;
+};
+
+function popupClose() {
+    popupElement.classList.remove('popup_is-open')
+};
+// Сохранение данных из формы и их замена
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    userName.textContent = nameInput.value;
+    userDescription.textContent = nameDescription.value;
+
+    popupClose();
+}
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+formsZone.addEventListener('submit', formSubmitHandler);
