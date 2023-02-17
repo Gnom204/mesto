f938a8d7a6f54a8984b22ee6b22b10210fc46682
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-02-17 04:46:34 +0300

    Updates

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const cssnanoPlugin = require('cssnano');
-
-module.exports = {
-    plugins: [
-        autoprefixer,
-        cssnanoPlugin({ preset: 'default' })
-    ]
-}
